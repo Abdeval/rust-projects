f339c55ee9760107f3ac41ef16b0453d1084f775
Abdeval <abdelatifreziga@gmail.com>
2025-08-20 09:58:27 +0100

    some changes

--- crate_learn/src/addition.rs
+++ crate_learn/src/addition.rs
@@ -1,3 +1,14 @@
+/// Add a to b
+///
+/// # Examples
+/// 
+/// ```
+/// let a = 2;
+/// let b = 2;
+/// let answer = crate_learn::addition::add(a, b);
+/// assert_eq!(4, answer);
+///```
+
 
 pub fn add(a: i32, b: i32) -> i32 {
     a + b
--- crate_learn/src/lib.rs
+++ crate_learn/src/lib.rs
@@ -1,5 +1,10 @@
-
+//! # This crate
+//!
+//! `crate_learn` is a collection of utilities to make performing certain
+//! calculations more convenient.
+/// adds two numbers ...etc
+/// 
 // ! declaration of the crate
 pub mod addition;
-pub mod multiplication;
 pub mod front_of_house;
+pub mod multiplication;
